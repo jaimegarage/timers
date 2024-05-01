63c5bfea93b4ea1837a8a459a2bb51d9f90592ba
Jaime Gaviria <72325369+jaimegarage@users.noreply.github.com>
2024-05-01 14:09:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 function iniciarTemporizador() {
-    var duracionInput = document.getElementById("duracion");
-    var duracion = parseInt(duracionInput.value) * 60; // Convertir minutos a segundos
+    var duraciones = document.querySelectorAll(".duracion");
+    duraciones.forEach(function(duracionInput) {
+        var duracion = parseInt(duracionInput.value) * 60; // Convertir minutos a segundos
 
-    if (isNaN(duracion) || duracion <= 0) {
-        alert("Por favor, ingresa una duración válida en minutos.");
-        return;
-    }
-
-    var temporizador = document.getElementById("temporizador");
-    var tiempo = duracion;
-
-    var intervalo = setInterval(function() {
-        var horas = Math.floor(tiempo / 3600);
-        var minutos = Math.floor((tiempo % 3600) / 60);
-        var segundos = tiempo % 60;
-        temporizador.textContent = 
-            (horas < 10 ? "0" : "") + horas + ":" +
-            (minutos < 10 ? "0" : "") + minutos + ":" +
-            (segundos < 10 ? "0" : "") + segundos;
-
-        if (tiempo === 0) {
-            clearInterval(intervalo); // Detener el temporizador cuando llegue a cero
-            alert("¡Tiempo terminado!");
-        } else {
-            tiempo--;
+        if (isNaN(duracion) || duracion <= 0) {
+            alert("Por favor, ingresa una duración válida en minutos.");
+            return;
         }
-    }, 1000);
+
+        var temporizador = duracionInput.nextElementSibling.nextElementSibling;
+        var tiempo = duracion;
+
+        var intervalo = setInterval(function() {
+            var horas = Math.floor(tiempo / 3600);
+            var minutos = Math.floor((tiempo % 3600) / 60);
+            var segundos = tiempo % 60;
+            temporizador.textContent = 
+                (horas < 10 ? "0" : "") + horas + ":" +
+                (minutos < 10 ? "0" : "") + minutos + ":" +
+                (segundos < 10 ? "0" : "") + segundos;
+
+            if (tiempo === 0) {
+                clearInterval(intervalo); // Detener el temporizador cuando llegue a cero
+                alert("¡Tiempo terminado!");
+            } else {
+                tiempo--;
+            }
+        }, 1000);
+    });
 }
